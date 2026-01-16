7cada6e1ec0081a56acf58d9b47c0d4fc1d5120c
Hamza <hamzazaidi253@gamil.com>
2026-01-16 09:32:42 +0100

    ok

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import Bank.Bank;
+
 class Main{
     static public  void main(){
-
+        Bank b = new Bank();
+        b.ajouterClient();
+        b.creerCompte();
+        b.creerCompte();
+        b.creerCompte();
+        b.supprimerClient();
     }
 
 }
